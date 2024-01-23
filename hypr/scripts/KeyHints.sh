f7ecd045420f08d362a8d64930d4d434aa5bbeae
formadi <bluesky4639@gmail.com>
2024-01-23 19:02:41 +0900

    update

--- hypr/scripts/KeyHints.sh
+++ hypr/scripts/KeyHints.sh
@@ -39,28 +39,33 @@ yad --width=$dynamic_width --height=$dynamic_height \
     --timeout-indicator=bottom \
 "ESC" "close this app" "" "=" "SUPER KEY (Windows Key)" "(SUPER KEY)" \
 " enter" "Terminal" "(kitty)" \
-" or  D" "App Launcher" "(rofi)" \
+"ALT" "App Launcher" "(rofi)" \
 " T" "Open File Manager" "(Thunar)" \
 " Q" "close active window" "(not kill)" \
 " Shift Q " "closes a specified window" "(window)" \
-" Alt V" "Clipboard Manager" "(cliphist)" \
+"Alt Va" "Clipboard Manager" "(cliphist)" \
+""\
 " W" "Choose wallpaper" "(Wallpaper Menu)" \
-"CTRL ALT W" "Random wallpaper" "(via swww)" \
+" ALT W" "Random wallpaper" "(via swww)" \
+""\
 " B" "Hide/UnHide Waybar" "waybar" \
 " CTRL B" "Choose waybar styles" "(waybar styles)" \
 " ALT B" "Choose waybar layout" "(waybar layout)" \
 " ALT R" "Reload Waybar swaync Rofi" "CHECK NOTIFICATION FIRST!!!" \
 " SHIFT N" "Launch Notification Panel" "swaync Notification Center" \
-" Print" "screenshot" "(grim)" \
-" Shift Print" "screenshot region" "(grim + slurp)" \
-" Shift S" "screenshot region" "(swappy)" \
-"ALT Print" "Screenshot active window" "active window only" \
+""\
+" SHIFT S" "screenshot"  "(fameshot - region)" \
+" Print" "screenshot" "(hyprshot - window)" \
+" Shift Print" "screenshot" "(hyprshot - region)" \
+" CTRL Print" "screenshot" "(hyprshot - fullscreen)" \
+" ALT Print" "screenshot" "(10s)" \
+""\
 "CTRL ALT P" "power-menu" "(wlogout)" \
 "CTRL ALT L" "screen lock" "(swaylock)" \
 "CTRL ALT Del" "Hyprland Exit" "(SAVE YOUR WORK!!!)" \
-" F" "Fullscreen" "Toggles to full screen" \
+" Z" "Fullscreen" "Toggles to full screen" \
 " ALT L" "Toggle Dwindle | Master Layout" "Hyprland Layout" \
-" Shift F" "Toggle float" "single window" \
+" A" "Toggle float" "single window" \
 " ALT F" "Toggle all windows to float" "all windows" \
 " Shift B" "Toggle Blur" "normal or less blur" \
 " SHIFT G" "Gamemode! All animations OFF or ON" "toggle" \
@@ -68,6 +73,3 @@ yad --width=$dynamic_width --height=$dynamic_height \
 " E" "View or EDIT Keybinds, Settings, Monitor" "" \
 "" "" "" \
 "More tips:" "https://github.com/JaKooLit/Hyprland-Dots/wiki" ""\
-
-
-
