9de987d5203aad4e4ec7a9a736c68e0ca5e24d1c
formadi <bluesky4639@gmail.com>
2024-02-04 16:13:31 +0900

    update

--- hypr/UserScripts/changeMasterLayout.sh
+++ hypr/UserScripts/changeMasterLayout.sh
@@ -2,31 +2,65 @@
 
 handle() {
     case $1 in
-        openwindow*|closewindow*|changefloatingmode*)
+        openwindow*|closewindow*)
             sleep 0.1
             workspace_info=$(hyprctl activeworkspace -j)
             workspace_id=$(echo "$workspace_info" | jq -r '.id')
             workspace_name=$(echo "$workspace_info" | jq -r '.name')
-
             clients_info=$(hyprctl clients -j)
             matching_windows=$(echo "$clients_info" | jq -r --arg workspace_id "$workspace_id" --arg workspace_name "$workspace_name" 'map(select(.workspace.id == ($workspace_id | tonumber) and .workspace.name == $workspace_name and .floating == false)) | length')
 
-            echo "Matching non-floating windows for workspace $workspace_name (ID: $workspace_id): $matching_windows"
+            # echo "Matching non-floating windows for workspace $workspace_name (ID: $workspace_id): $matching_windows"
 
             case $matching_windows in
                 1)
-                    hyprctl dispatch "layoutmsg mfact 0.65" ; hyprctl dispatch "layoutmsg orientationcenter" ; hyprctl reload
+                    hyprctl dispatch "layoutmsg mfact 0.65" ; hyprctl dispatch "layoutmsg orientationcenter" # ; hyprctl reload
                     ;;
                 2)
-                    # 추가적인 조건에 따라 수행할 동작
-                    # 예: hyprctl keyword general:some_other_command
-                    hyprctl dispatch "layoutmsg mfact 0.5" ; hyprctl dispatch "layoutmsg orientationleft" ; hyprctl reload
+                    hyprctl dispatch "layoutmsg mfact 0.5"  ; hyprctl dispatch "layoutmsg orientationright"  # ; hyprctl reload
                     ;;
                 *)
-                    hyprctl dispatch "layoutmsg mfact 0.45" ; hyprctl dispatch "layoutmsg orientationcenter" ; hyprctl reload
+                    hyprctl dispatch "layoutmsg mfact 0.45" ; hyprctl dispatch "layoutmsg orientationcenter" # ; hyprctl reload
                     ;;
             esac
             ;;
+
+        changefloatingmode* )
+            workspace_info=$(hyprctl activeworkspace -j)
+            workspace_id=$(echo "$workspace_info" | jq -r '.id')
+            workspace_name=$(echo "$workspace_info" | jq -r '.name')
+            clients_info=$(hyprctl clients -j)
+            matching_windows=$(echo "$clients_info" | jq -r --arg workspace_id "$workspace_id" --arg workspace_name "$workspace_name" 'map(select(.workspace.id == ($workspace_id | tonumber) and .workspace.name == $workspace_name and .floating == false)) | length')
+
+            # echo "Matching non-floating windows for workspace $workspace_name (ID: $workspace_id): $matching_windows"
+
+            case $matching_windows in
+                1)
+                    hyprctl dispatch "layoutmsg mfact 0.65" ; hyprctl dispatch "layoutmsg orientationcenter" #  ; hyprctl reload
+                    ;;
+                2)
+                    hyprctl dispatch "layoutmsg mfact 0.5"  ; hyprctl dispatch "layoutmsg orientationright"  # ; hyprctl reload
+                    ;;
+                *)
+                    hyprctl dispatch "layoutmsg mfact 0.45" ; hyprctl dispatch "layoutmsg orientationcenter" # ; hyprctl reload
+                    ;;
+            esac
+
+            # changefloatingmode에 대한 처리
+            mode=$(echo "$1" | awk -F',' '{print $NF}')
+            if [ "$mode" = "1" ]; then
+                # echo "Changing floating mode to 1"
+                # 크기를 변경
+                hyprctl dispatch "centerwindow" ; hyprctl dispatch "resizeactive exact 2000 1600"
+
+            # elif [ "$mode" = "0" ]; then
+                # echo "Changing floating mode to 0"
+            # else
+                # echo "Invalid mode: $mode"
+            fi
+        # *)
+        #     # echo "undefined command: $1"
+        #     exit 0
     esac
 }
 
